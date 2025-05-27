79a711e89b16aef969631e336d79f335464aa4f6
Darwis <darwis.e03@gmail.com>
2025-05-27 09:57:19 +0800

    try custom update

--- diy-part8-diy.sh
+++ diy-part8-diy.sh
@@ -8,7 +8,7 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#patches
+# patches
 wget https://github.com/quintus-lab/openwrt-rockchip/raw/21.02/patches/1001-dnsmasq-add-filter-aaaa-option.patch
 wget https://github.com/quintus-lab/openwrt-rockchip/raw/21.02/patches/1003-luci-app-firewall_add_fullcone.patch
 wget https://github.com/quintus-lab/openwrt-rockchip/raw/21.02/patches/1002-add-fullconenat-support.patch
@@ -42,44 +42,48 @@ cp -rf $GITHUB_WORKSPACE/patchs/xiaomi_mi-router/mt7621_xiaomi_mi-router-4a-giga
 cp -rf $GITHUB_WORKSPACE/patchs/5.4/mt76x8/dts/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/dts/
 cp -rf $GITHUB_WORKSPACE/patchs/5.4/mt76x8/mt76x8.mk $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/mt76x8.mk
 cp -rf $GITHUB_WORKSPACE/patchs/5.4/mt76x8/02_network $GITHUB_WORKSPACE/openwrt/target/linux/ramips/mt76x8/base-files/etc/board.d/02_network
-#备用方案
+# 备用方案
 sed -i 's/git.openwrt.org\/project\/luci.git;openwrt-21.02/github.com\/coolsnowwolf\/luci.git;master/g' feeds.conf.default
 sed -i 's/git.openwrt.org\feed\/packages.git;openwrt-21.02/github.com\/coolsnowwolf\/packages.git;master/g' feeds.conf.default
 
-# 增加软件包
+# Add software package
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small.git;master' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' feeds.conf.default
 
-# 修改默认dnsmasq为dnsmasq-full
+# Modify the default dnsmasq to dnsmasq-full
 sed -i 's/dnsmasq/dnsmasq-full firewall iptables/g' include/target.mk
 
-# 修改默认编译LUCI进系统
-sed -i 's/ppp-mod-pppoeipset ip-full ppp-mod-pppoe default-settings luci curl ca-certificates/g' include/target.mk
+# DRW -> test later
+# Modify the default compilation of LUCI into the system
+# sed -i 's/ppp-mod-pppoeipset ip-full ppp-mod-pppoe default-settings luci curl ca-certificates/g' include/target.mk
 
-# 修改默认红米AC2100 wifi驱动为闭源驱动
-sed -i 's/kmod-mt7603 kmod-mt7615e kmod-mt7615-firmware/kmod-mt7603e kmod-mt7615d luci-app-mtwifi -wpad-openssl/g' target/linux/ramips/image/mt7621.mk
+# DRW -> test later
+# Modify the default Redmi AC2100 wifi driver to a closed source driver
+# sed -i 's/kmod-mt7603 kmod-mt7615e kmod-mt7615-firmware/kmod-mt7603e kmod-mt7615d luci-app-mtwifi -wpad-openssl/g' target/linux/ramips/image/mt7621.mk
 
-# 修改默认小米路由4A千兆版 wifi驱动为闭源驱动，更改闪存分区大小
-sed -i 's/kmod-mt7603 kmod-mt76x2/kmod-mt7603e kmod-mt76x2e luci-app-mtwifi -wpad-openssl/g' target/linux/ramips/image/mt7621.mk
-sed -i '1509s/IMAGE_SIZE := 14848k/IMAGE_SIZE := 16064k/' target/linux/ramips/image/mt7621.mk
+# DRW -> test later
+# Modify the default Xiaomi Router 4A Gigabit Edition Wi-Fi driver to a closed-source driver and change the flash partition size
+# sed -i 's/kmod-mt7603 kmod-mt76x2/kmod-mt7603e kmod-mt76x2e luci-app-mtwifi -wpad-openssl/g' target/linux/ramips/image/mt7621.mk
+# sed -i '1509s/IMAGE_SIZE := 14848k/IMAGE_SIZE := 16064k/' target/linux/ramips/image/mt7621.mk
 
-# 设置闭源驱动开机自启
-sed -i '2a ifconfig rai0 up\nifconfig ra0 up\nbrctl addif br-lan rai0\nbrctl addif br-lan ra0' package/base-files/files/etc/rc.local
+# DRW -> test later
+# Set closed-source driver to start automatically at boot
+# sed -i '2a ifconfig rai0 up\nifconfig ra0 up\nbrctl addif br-lan rai0\nbrctl addif br-lan ra0' package/base-files/files/etc/rc.local
 
-# 单独拉取软件包
-git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/default-settings
-git clone -b lua5.4 https://github.com/yuos-bit/other package/lua5.4
-git clone -b debug https://github.com/yuos-bit/luci-theme-edge2 package/luci-theme-edge2
-git clone -b mipsle-xray https://github.com/yuos-bit/other package/passwall
-git clone -b main https://github.com/yuos-bit/other package/main
+# Pull the package individually
+# git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/default-settings
+# git clone -b lua5.4 https://github.com/yuos-bit/other package/lua5.4
+# git clone -b debug https://github.com/yuos-bit/luci-theme-edge2 package/luci-theme-edge2
+# git clone -b mipsle-xray https://github.com/yuos-bit/other package/passwall
+# git clone -b main https://github.com/yuos-bit/other package/main
 
-# 更改默认wifi
+# Change default wifi
 # cp -rf $GITHUB_WORKSPACE/patchs/NX30Pro/mtwifi.sh package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 
-# 更新openssl3.0
+# Update openssl 3.0
 rm -rf package/libs/openssl
 git clone -b openssl https://github.com/yuos-bit/other package/openssl
 cp -rf $GITHUB_WORKSPACE/patchs/immortalwrt-mt798x/openssl-module.mk $GITHUB_WORKSPACE/openwrt/include/openssl-module.mk
--- diy-part9.sh
+++ diy-part9.sh
@@ -10,62 +10,62 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #=================================================
 
-# 修改默认IP
-sed -i 's/192.168.1.1/10.32.0.1/g' package/base-files/files/bin/config_generate
-# 修改网关
-sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
+# Default IP
+# sed -i 's/192.168.1.1/10.32.0.1/g' package/base-files/files/bin/config_generate
+# Gateway
+# sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
 
-# 修改主机名称
-sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
+# hostname
+# sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
 
-# 修改默认wifi名称ssid为Xiaoyu-Wifi
-sed -i 's/ssid=OpenWrt/ssid=Xiaomi-Wifi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# Ubah default SSID
+# sed -i 's/ssid=OpenWrt/ssid=Xiaomi-Wifi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 修改默认wifi密码key为1234567890
+# Ubah wifi password default -> 1234567890
 sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-#使用sed 在第四行后添加新字
+# sed untuk menambahkan setelah baris ke 4
 sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_USER="MOLUN"' >>.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"MOLUN"@' .config
+    echo 'CONFIG_KERNEL_BUILD_USER="DARWIS"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"DARWIS"@' .config
 
 # Add kernel build domain
 [ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
     echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
     sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
 
-
+# DRW -> test later
 # nft-fullcone
 git clone -b main --single-branch https://github.com/fullcone-nat-nftables/nftables-1.0.5-with-fullcone package/nftables
 git clone -b master --single-branch https://github.com/fullcone-nat-nftables/libnftnl-1.2.4-with-fullcone package/libnftnl
 
-# 打补丁
+# patch nft-fullcone
 wget -O package/firmware/xt_FULLCONENAT.c https://raw.githubusercontent.com/Chion82/netfilter-full-cone-nat/master/xt_FULLCONENAT.c
 cp -rf package/firmware/xt_FULLCONENAT.c package/nftables/include/linux/netfilter/xt_FULLCONENAT.c
 cp -rf package/firmware/xt_FULLCONENAT.c package/libnftnl/include/linux/netfilter/xt_FULLCONENAT.c
 cp -rf package/firmware/xt_FULLCONENAT.c package/libs/libnetfilter-conntrack/xt_FULLCONENAT.c
 
-# 测试编译时间
+# Compile time
 YUOS_DATE="$(date +%Y.%m.%d)(测试版)"
 BUILD_STRING=${BUILD_STRING:-$YUOS_DATE}
 echo "Write build date in openwrt : $BUILD_DATE"
-echo -e '\n小渔学长 Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
+echo -e '\nDarwis Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
 sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
 echo "DISTRIB_REVISION=''" >> package/base-files/files/etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='小渔学长 Build @ ${BUILD_STRING}'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='Darwis Build @ ${BUILD_STRING}'" >> package/base-files/files/etc/openwrt_release
 sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
 
-# dnsmasq-full升级
+# dnsmasq-full upgrade
 rm -rf package/network/services/dnsmasq/*
 cp -rf $GITHUB_WORKSPACE/patchs/5.4/dnsmasq/* $GITHUB_WORKSPACE/openwrt/package/network/services/dnsmasq/
 
 
-#升级golang
+# upgrade golang
 find . -type d -name "golang" -exec rm -r {} +
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 19.x feeds/packages/lang/golang
